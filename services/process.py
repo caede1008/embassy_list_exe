4523f0fe2c64c51656efa96c27be3fc9f71afb43
caede1008 <79036121+caede1008@users.noreply.github.com>
2023-01-29 18:19:37 +0900

    Add files via upload

---
+++ services/process.py
@@ -0,0 +1,7 @@
+# process.py
+class Process:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def goPage(self):
+        self.driver.get("https://www.mofa.go.jp/mofaj/link/emblist/europe.html")
